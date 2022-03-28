f96cf2067c2deae8ddfcaf0c1b231323832e0fde
capmahin <mahinulhuque1996@gmail.com>
2022-03-29 04:50:57 +0600

    add routes and route path and element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Friends from "./components/Friends/Friends";
+import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Welcome to my fancy routing website</h1>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/Friends" element={<Friends />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
